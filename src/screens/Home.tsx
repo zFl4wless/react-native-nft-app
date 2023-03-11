3e5951b42b4f186a218b927b09a088e6e10fbd23
zfl4wless <noah.bauditz@live.de>
2023-03-11 16:25:35 +0100

    refactor: moves assets & constants to src folder

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,14 @@
-import { Text } from 'react-native';
+import { useState } from 'react';
+import { View, SafeAreaView, FlatList, Text } from 'react-native';
+
+import { COLORS, NFTData } from '../constants';
 
 const Home = () => {
-    return <Text>Home</Text>;
+    return (
+        <SafeAreaView style={{flex: 1}}>
+
+        </SafeAreaView>
+    );
 };
 
 export default Home;
