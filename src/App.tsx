82297999ce76b107ac515c6bf99f9bdd581dca0b
zfl4wless <noah.bauditz@live.de>
2023-03-11 11:40:47 +0100

    feat: adds assets & loads custom fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import Home from './screens/Home';
 import Details from './screens/Details';
 
@@ -14,6 +15,16 @@ const theme = {
 };
 
 const App = () => {
+    const [loaded] = useFonts({
+        InterBold: require('./assets/fonts/Inter-Bold.ttf'),
+        InterSemibold: require('./assets/fonts/Inter-SemiBold.ttf'),
+        InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
+        InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
+        InterLight: require('./assets/fonts/Inter-Light.ttf'),
+    });
+
+    if (!loaded) return null;
+
     return (
         <NavigationContainer theme={theme}>
             <Stack.Navigator
